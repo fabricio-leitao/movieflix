02985d0c82f4e07cf87490b730eab5cbecc663b1
Fabricio Ramos <fabricio-beco@hotmail.com>
2021-05-06 17:21:38 -0300

    ButtonIcon e Login implementados

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import './App.scss';
 import './core/assets/custom.scss';
 import Navbar from './core/components/Navbar';
+import Login from './pages/Auth/Login';
 
 const App = () => {
   return (
+    <>
     <Navbar/>
+    <Login/>
+    </>
   );
 }
 
