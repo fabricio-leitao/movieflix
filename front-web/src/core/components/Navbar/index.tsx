bc212e5b4485db654132a216c679d99ce1ed0025
Fabricio Ramos <fabricio-beco@hotmail.com>
2021-05-06 16:54:20 -0300

    Navbar implementado

---
+++ front-web/src/core/components/Navbar/index.tsx
@@ -0,0 +1,9 @@
+import './styles.scss'
+
+const Navbar = () => (
+    <nav className="nav bg-primary main-nav">
+        <h1 className="main-nav-text">MovieFlix</h1>
+    </nav>
+);
+
+export default Navbar;
