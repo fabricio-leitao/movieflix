8663b9b19354aa975505f95f7f217f19f1276cdb
Fabricio Ramos <fabricio-beco@hotmail.com>
2021-05-22 15:00:56 -0300

    finalizando

--- front-web/src/Routes.tsx
+++ front-web/src/Routes.tsx
@@ -1,24 +1,26 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from "./core/components/Navbar";
+import PrivateRoute from "./core/components/Routes/PrivateRoute";
 import Auth from "./pages/Auth";
 import MovieCatalog from "./pages/MovieCatalog";
 import MovieDetails from "./pages/MovieCatalog/components/MovieDetails";
+import history from "./core/utils/history"
 
 const Routes = () => {
     return (
         <BrowserRouter>
         <Navbar/>
         <Switch>
-        <Route path="/movies/:movieId">
-                <MovieDetails/>
-            </Route>
-            <Route path="/movies">
-                <MovieCatalog/>
-            </Route>
-            <Route path="/">
+        <PrivateRoute path="/movies/:movieId">
+            <MovieDetails/>
+        </PrivateRoute>
+        <PrivateRoute path="/movies">
+            <MovieCatalog/>
+        </PrivateRoute>
+        <Route path="/">
                 <Auth/>
-            </Route>
+        </Route>
         </Switch>
     </BrowserRouter>
     );
--- front-web/src/core/components/Navbar/__teste__/Navbar.spec.tsx
+++ front-web/src/core/components/Navbar/__teste__/Navbar.spec.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Navbar from '..';
-
 test('should render Navbar', () => {
     render(
         <Navbar/>
